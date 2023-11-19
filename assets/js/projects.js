de5aad8bdb88ce26184460e7515b9af3ad9f6e48
ragna-andersson <ragna.andersson@vrenninge.se>
2023-11-19 19:22:04 +0100

    Javascript för att visa projekt

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -4,7 +4,7 @@ const projects = [
             rubrik: "Reseplaneraren - Lättanvänd?",
             body: "Kan UX-research användas för att förbättra användarupplevelsen av Västtrafiks Reseplanerare? Vilken viktig information framkommer från undersökningen för att optimera tjänsten?",
             img: "/assets/img/projekt/prj1_hearts.png",
-            url: ""
+            url: "cv.html"
         },
 
         {
@@ -30,23 +30,32 @@ const projects = [
 
     ];
 
+
 function populateProjects() {
    
-    const vartProjectSkaLigga = document.getElementById("project");
+    const vartProjectSkaLigga = document.getElementById("projects");
 
-    var peopleHTML = "<div class='persons'>";
-    for (let i = 0; i < people.length; i++) {
-        const person = people[i];
-        peopleHTML += `
-            <div class="person">
-                <img src="${person.img}" alt="${person.name}">
-                <h3>${person.name}</h3>
-                <p>${person.title}</p>
+    var projectHTML = ""
+    for (let i = 0; i < projects.length; i++) {
+        const project = projects[i];
+        // style används för att varje projekt ska få en egen rad. (Första projektet får grid-row: 2)
+        projectHTML += `
+        <article class="col1-2 grid-container-2-col" style="grid-row:${i+2};"> 
+            <img class="col1 project_img" src="${project.img}" alt="screenshot of västrafik app" />
+
+            <div class="col2">
+                <h3 class="left-align">${project.rubrik}</h3>
+                <p>${project.body}</p>
+                <a class="project_button" href="${project.url}">Läs mer 
+                    <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z"/>
+                    </svg>
+                </a>
             </div>
+        </article>
         `;
     }
-    peopleHTML += "</div>";
-    vartPeopleDivskaligga.innerHTML = peopleHTML;
+    vartProjectSkaLigga.innerHTML += projectHTML;
 }
 
-populateContactUs();
+populateProjects();
